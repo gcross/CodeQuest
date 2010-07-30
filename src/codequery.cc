1b248364ad2ffed59e4c424e8920f2ac016c3505
Gregory Crosswhite <gcross@phys.washington.edu>
2010-07-29 18:58:53 -0700

    Polished the explanation message;  changed default format from sparse to dense.

--- src/codequery.cc
+++ src/codequery.cc
@@ -17,18 +17,18 @@ extern int main_sparse(string filename, bool compute_weights_flag);
 int main(int argc, char** argv) {
 
 try {
-	CmdLine cmd("codequery description message", ' ', "1.0 beta");
+	CmdLine cmd("codequery description message", ' ', "1.0");
 
     vector<string> allowed_formats;
     allowed_formats.push_back("dense");
     allowed_formats.push_back("sparse");
     ValuesConstraint<string> allowedVals( allowed_formats );
-    ValueArg<string> formatArg("f","format","Input/Output format of quantum operators",false,"sparse",&allowedVals,cmd);
+    ValueArg<string> formatArg("f","format","Input and output format of quantum operators;  if no format is specified, then defaults to dense.",false,"dense",&allowedVals,cmd);
 
-	SwitchArg weightsSwitch("w","weights","Compute the minimum weights (very expensive!)", false);
+	SwitchArg weightsSwitch("w","weights","Compute the optimal minimum distances for the logical qubits (very expensive!).  Note that this will result in a different (but equivalent) set of logical operators.", false);
     cmd.add(weightsSwitch);
 
-    UnlabeledValueArg<string> filenameArg("filename","Input file with quantum operators",false,"","filename",cmd);
+    UnlabeledValueArg<string> filenameArg("filename","Input file with quantum operators;  if no file is specified, then defaults to standard input.",false,"","filename",cmd);
 
 	cmd.parse( argc, argv );
 
