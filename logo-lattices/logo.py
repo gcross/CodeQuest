19446e5f312e2fd5bdc8204d032329d0e5f10dfd
gcross <gcross@phys.washington.edu>
2010-09-11 14:38:48 -0700

    Add documentation and remove logo-lattices for release.

--- logo-lattices/logo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#@+leo-ver=4-thin
-#@+node:gmc.20080914232002.19:@thin logo.py
-#@@first
-#@@language python
-from tilings_interpreter import main
-main()
-#@-node:gmc.20080914232002.19:@thin logo.py
-#@-leo
